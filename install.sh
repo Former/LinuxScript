d0d137f37c8a62da363871f135f75a0f70cfc02f
Alexei Bezborodov <AlexeiBv@yandex.ru>
2022-04-04 23:52:55 +0300

    2022

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo aptitude install chromium-browser gimp geany geany-plugins screen \
+ gcc virtualbox-qt virtualbox-ext-pack icewm idesk dillo links2 smtube \
+ vlc mpv mc openssh-server mplayer-gui texstudio volumeicon-alsa \
+ p7zip-full git falkon ffmpeg remmina remmina-plugin-rdp xrdp \
+ remmina-plugin-vnc nmap gparted lxterminal redshift stellarium \
+ minetest widelands chromium-browser-l10n winehq-stable sshfs encfs\
+ pcmanfm xclip clang clang-4.0 clang-3.9 gitg gitk git-cola \
+ texlive-full pwgen mmv cheese gxneur xneur tuxguitar audacious \
+ deluge easystroke
