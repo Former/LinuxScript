0425db1647884e9eee3ee8c69e1b4411975eb8a6
Alexei Bezborodov <AlexeiBv@yandex.ru>
2022-04-23 22:33:35 +0300

    Install wine

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ sudo aptitude install chromium-browser gimp geany geany-plugins screen \
  vlc mpv mc openssh-server mplayer-gui texstudio volumeicon-alsa \
  p7zip-full git falkon ffmpeg remmina remmina-plugin-rdp xrdp \
  remmina-plugin-vnc nmap gparted lxterminal redshift stellarium \
- minetest widelands chromium-browser-l10n winehq-stable sshfs encfs\
+ minetest widelands chromium-browser-l10n sshfs encfs\
  pcmanfm xclip clang clang-4.0 clang-3.9 gitg gitk git-cola \
  texlive-full pwgen mmv cheese gxneur xneur tuxguitar audacious \
  deluge easystroke
+
+sudo apt install --install-recommends winehq-stable
