86f8f558dd4ff4ef0adba7f54c01d959bd6309a7
Alexei <AlexeiBv+x63s@narod.ru>
2022-12-05 19:59:20 +0300

    Update install

--- install.sh
+++ install.sh
@@ -15,8 +15,7 @@ sudo aptitude install chromium-browser gimp geany geany-plugins screen \
 sudo apt install --install-recommends winehq-stable
 
 # Alt Linux
-sudo systemctl enable sshd.service
-sudo systemctl start sshd.service
+sudo systemctl enable --now sshd.service
 
 sudo apt-get update
 sudo apt-get dist-upgrade
@@ -26,26 +25,22 @@ sudo apt-get install gimp geany geany-plugins screen gcc icewm idesk \
  dillo links2 smtube vlc mpv mc openssh-server mplayer-gui texstudio \
  volumeicon git ffmpeg remmina xrdp nmap gparted redshift stellarium  \
  minetest widelands sshfs encfs pcmanfm xclip clang gitg gitk git-cola \
- texlive pwgen cheese gxneur xneur audacious deluge  gnome-disk-utility
-
-sudo apt-get install cinnamon systemd-settings-disable-kill-user-processes \
- firefox xviewer gnome-system-monitor filezilla baobab gedit
- 
-sudo apt-get install nemo nemo-emblems nemo-fileroller \
+ texlive pwgen cheese gxneur xneur audacious deluge  gnome-disk-utility \
+ cinnamon-full systemd-settings-disable-kill-user-processes \
+ firefox xviewer gnome-system-monitor filezilla baobab gedit \
+ nemo nemo-emblems nemo-fileroller \
  nemo-compare nemo-share-common nemo-arronax nemo-preview nemo-python-devel \
  nemo-extensions-translations nemo-translations nemo-terminal nemo-share \
- nemo-image-converter nemo-python nemo-preview-gir-devel nemo-sendto-clamtk
-
-sudo apt-get install golang sqlite3 nodejs
+ nemo-image-converter nemo-python nemo-preview-gir-devel nemo-sendto-clamtk \
+ golang sqlite3 nodejs
 
 sudo eepm play skype
 sudo eepm play telegram
 sudo eepm play zoom
 
-wget https://packages.ntop.org/centos-stable/8/x86_64/Packages/n2n-2.8.0-540.x86_64.rpm
-sudo apt-get install ./n2n-2.8.0-540.x86_64.rpm
+wget https://packages.ntop.org/centos-stable/8/x86_64/Packages/n2n-3.0.0-1044.x86_64.rpm
+sudo apt-get install ./n2n-3.0.0-1044.x86_64.rpm
 
-sudo systemctl enable edge.service
-sudo systemctl start edge.service
+sudo systemctl enable --now edge.service 
 
 git config --global core.editor mcedit
