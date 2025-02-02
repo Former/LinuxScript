438fff5e5dfa10b1b69ea442a03944dc77d08dc7
Alexei Bezborodov <alexeibv+git3@narod.ru>
2025-02-02 12:26:09 +0300

    wl520 minimal usb

--- OpenWrt/make_image_wl520gu.bash
+++ OpenWrt/make_image_wl520gu.bash
@@ -2,7 +2,8 @@
 
 #sudo apt-get install subversion build-essential libncurses5-dev zlib1g-dev gawk git ccache gettext libssl-dev xsltproc
 
-image_builder_url="https://downloads.openwrt.org/releases/22.03.7/targets/bcm47xx/legacy/openwrt-imagebuilder-22.03.7-bcm47xx-legacy.Linux-x86_64.tar.xz"
+image_builder_url="https://downloads.openwrt.org/releases/18.06.9/targets/brcm47xx/legacy/openwrt-imagebuilder-18.06.9-brcm47xx-legacy.Linux-x86_64.tar.xz"
+#"https://downloads.openwrt.org/releases/22.03.7/targets/bcm47xx/legacy/openwrt-imagebuilder-22.03.7-bcm47xx-legacy.Linux-x86_64.tar.xz"
 
 file_name="${image_builder_url##*/}"
 base="${file_name%.[^.]*.[^.]*}"
@@ -19,9 +20,10 @@ echo ${file_name} ${base} ${ext}
 
 cd ${image_builder_dir}
 
-#make image PROFILE= PACKAGES="-lcui" FILES=files/
+make image PROFILE=asus-wl-520gu PACKAGES="-lcui -ppp -ppp-mod-pppoe base-files busybox dnsmasq -dropbear -firewall fstools -ip6tables iptables kernel -kmod-b43 -kmod-gpio-button-hotplug -kmod-ipt-offload -kmod-leds-gpio -kmod-ledtrig-default-on -kmod-ledtrig-netdev -kmod-ledtrig-timer libc libgcc logd -mtd netifd nvram -odhcp6c -odhcpd-ipv6only opkg otrx swconfig uci uclient-fetch -wpad-mini block-mount kmod-usb-storage kmod-fs-ext4 kmod-usb-ohci kmod-usb2"
+# FILES=files/
 
-make image
+#make image
 
 make info
 
