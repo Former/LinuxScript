6e4e55e68da7c5b6dca9fcadc699511208221f34
Alexei Bezborodov <alexeibv+git3@narod.ru>
2026-03-06 16:40:09 +0300

    lang_sw

---
+++ lang_swich.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Copyright 2025 by Alexei Bezborodov <AlexeiBv+linux_script@narod.ru> 
+# License: Public domain: http://unlicense.org/ 
+# Общественное достояние 
+
+#!/bin/bash
+
+source ~/.bashrc
+
+lang_buffer_switch
